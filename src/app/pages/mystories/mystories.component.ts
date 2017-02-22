cd0b65c2e76a7bf0fea9765abbddefca71299a94
byambaa122 <byambaa.122@gmail.com>
2017-02-22 19:17:06 +0800

    add MyNiceThings

--- src/app/pages/mystories/mystories.component.ts
+++ src/app/pages/mystories/mystories.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AfService } from "../../providers/af.service";
+import { FirebaseListObservable } from "angularfire2";
 
 @Component({
-  selector: 'app-mystories',
-  templateUrl: './mystories.component.html',
-  styleUrls: ['./mystories.component.css']
+	selector: 'app-mystories',
+	templateUrl: './mystories.component.html',
+	styleUrls: ['./mystories.component.css']
 })
 export class MystoriesComponent implements OnInit {
 
-  constructor() { }
+	public stories: any;
+	
+	constructor(public afService: AfService) {
 
-  ngOnInit() {
-  }
+	}
+
+	ngOnInit() {
+		this.stories = this.afService.af.database.list('/stories');
+	}
 
 }
