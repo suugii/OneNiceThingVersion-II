3207627343db7cab089d090f290994dfcd6316a6
N. Tugsbileg <N. Tugsbileg>
2017-02-24 09:57:51 +0800

    f

--- src/app/pages/contact-page/contact.ts
+++
@@ -1,9 +0,0 @@
-export class Contact {
-	constructor(
-		public name: string = "",
-		public email: string = "",
-		public message: string = "",
-		public created_at: any = Date.now(),
-		public updated_at: any = Date.now(),
-	) {  }
-}
