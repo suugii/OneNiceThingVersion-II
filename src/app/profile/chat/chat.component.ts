9b4c41d0979d35b56eb226a94ce24a67497a0db5
N. Tugsbileg <N. Tugsbileg>
2017-03-02 18:58:48 +0800

    working on chat

--- src/app/profile/chat/chat.component.ts
+++ src/app/profile/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from './../../service/auth.service';
 import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import * as _ from 'lodash';
@@ -10,7 +10,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 	styleUrls: ['./chat.component.css']
 })
 
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
 	public users: any[] = [];
 	public senderID: any;
 	public messages: any;
@@ -19,7 +19,7 @@ export class ChatComponent implements OnInit {
 	public currentUser: any;
 	public msgVal: string = '';
 	public limit: BehaviorSubject<number> = new BehaviorSubject<number>(10);
-	@ViewChild(".search-results") searchResult: ElementRef;
+	@ViewChild('scrollMe') private myScrollContainer: ElementRef;
 	constructor(public authservice: AuthService, public af: AngularFire) {
 		this.af.auth.subscribe(auth => {
 			if (auth) {
@@ -36,13 +36,20 @@ export class ChatComponent implements OnInit {
 
 	}
 
+	ngAfterViewChecked() {
+		this.scrollToBottom();
+	}
+	scrollToBottom(): void {
+		try {
+			this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+		} catch (err) { console.log('Scroll to bottom failed yo!') }
+	}
+	
 	scrolled(): void {
 		this.limit.next(this.limit.getValue() + 10);
 	}
 
-	onScrollUp() {
-		this.scrolled();
-	}
+
 
 	getUser(data) {
 		this.messages = '';
@@ -56,14 +63,25 @@ export class ChatComponent implements OnInit {
 		this.messages.subscribe(snapshots => {
 			this.userMessages = [];
 			snapshots.forEach(snapshot => {
-				if ((snapshot.senderID == this.senderID && snapshot.userID == this.currentUser.uid) || (snapshot.senderID ==this.currentUser.uid && snapshot.userID == this.senderID)) {
+				if ((snapshot.senderID == this.senderID && snapshot.userID == this.currentUser.uid) || (snapshot.senderID == this.currentUser.uid && snapshot.userID == this.senderID)) {
 					this.userMessages.push(snapshot);
 				}
 			});
 		})
 	}
 
-
+	isYou(email) {
+		if (email == this.currentUser.auth.email)
+			return true;
+		else
+			return false;
+	}
+	isMe(email) {
+		if (email == this.currentUser.auth.email)
+			return false;
+		else
+			return true;
+	}
 
 	chatSend(theirMessage: string) {
 		this.messages.push({ message: theirMessage, senderID: this.senderID, userID: this.currentUser.uid, name: this.currentUser.auth.email, date: Date.now() });
