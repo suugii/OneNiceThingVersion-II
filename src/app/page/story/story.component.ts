de1b0d5d03dffac0dc2fa4dd6a7bbe42f4f46070
N. Tugsbileg <N. Tugsbileg>
2017-03-09 13:09:58 +0800

    add map in story

--- src/app/page/story/story.component.ts
+++ src/app/page/story/story.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { StoryService } from "../../service/story.service";
+import { AuthService } from "../../service/auth.service";
 
 @Component({
 	selector: 'app-story',
@@ -15,19 +16,42 @@ export class StoryComponent implements OnInit {
 	favorites: FirebaseListObservable<any[]>;
 	object: FirebaseObjectObservable<any>;
 
-	story: any;	
+	story: any;
 	counter: number = 0;
 	favorited: boolean = false;
-	user: string;
+	user: any;
 
-	constructor(private router: Router, private route: ActivatedRoute, private af: AngularFire, private storyService: StoryService) {
-	    this.af.auth.subscribe(
-	        (auth) => {
-	            if (auth) {
-	                this.user = auth.uid;
-	            }
-	        }
-	    );
+	lat: number = 47.919991;
+	lng: number = 106.917708;
+	latA: number = 47.907018;
+	lngA: number = 106.908609;
+	zoom: number = 13;
+
+	iconURL: any = {
+		url: "http://www.famousbirthdays.com/headshots/ne-yo-3.jpg",
+		scaledSize: {
+			height: 40,
+			width: 40
+		}
+	};
+	iconURL1: any = {
+		url: "http://www.famousbirthdays.com/headshots/ne-yo-3.jpg",
+		scaledSize: {
+			height: 40,
+			width: 40
+		}
+	}
+
+
+	constructor(public authService: AuthService, private router: Router, private route: ActivatedRoute, private af: AngularFire, private storyService: StoryService) {
+		this.authService.af.auth.subscribe(
+			(auth) => {
+				if (auth) {
+					this.user = auth.uid;
+				}
+			}
+		);
+
 		this.favorites = this.af.database.list('favorites', {
 			query: {
 				orderByChild: 'sid',
@@ -36,31 +60,35 @@ export class StoryComponent implements OnInit {
 		});
 		this.object = this.af.database.object('stories' + '/' + this.key);
 
-        this.object.subscribe(
-            (object) => {
-		       	this.favorites.subscribe(
-		        	dataFav => {
-		        		dataFav.forEach(
-		        			favorite => {
-	        					this.counter = this.counter + 1;
-	        					if (favorite.uid == this.user) {
-	        						this.favorited = true;
-	        					}
-		        			}
-		        		)
-		        		object.favorite = this.counter;
-				        this.counter = 0;
-	        			object.favorited = this.favorited;
-	        			this.favorited = false;
-		        	}
-		        )
-                object.user = this.af.database.object('users' + '/' + object.user);
-                object.touser = this.af.database.object('users' + '/' + object.touser);
-                this.story = object;
-            }
-        );
+		this.object.subscribe(
+			(object) => {
+				this.favorites.subscribe(
+					dataFav => {
+						dataFav.forEach(
+							favorite => {
+								this.counter = this.counter + 1;
+								if (favorite.uid == this.user) {
+									this.favorited = true;
+								}
+							}
+						)
+						object.favorite = this.counter;
+						this.counter = 0;
+						object.favorited = this.favorited;
+						this.favorited = false;
+					}
+				)
+				object.user = this.af.database.object('users' + '/' + object.user);
+				object.touser = this.af.database.object('users' + '/' + object.touser);
+				this.story = object;
+			}
+		);
 	}
 
-	ngOnInit() { }
+	ngOnInit() {
+
+	}
+
+
 
 }
--- src/app/profile/user-home/user-home.component.ts
+++ src/app/profile/user-home/user-home.component.ts
@@ -7,28 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHomeComponent implements OnInit {
 
-    lat: number = 47.919991;
-    lng: number = 106.917708;
-    latA: number = 47.907018;
-    lngA: number = 106.908609;
-    latB: number = 47.911462;
-    lngB: number = 106.964983;
     zoom: number = 13;
     
-    iconURL: any = {
-    	url: "https://lh6.googleusercontent.com/-ZmFd6FCjwiQ/AAAAAAAAAAI/AAAAAAAAB_k/VL_7wG3-LoY/photo.jpg",
-	    scaledSize: {
-			height: 40,
-			width: 40
-	    }
-    };
-    iconURL1: any = {
-    	url: "http://www.famousbirthdays.com/headshots/ne-yo-3.jpg",
-	    scaledSize: {
-			height: 40,
-			width: 40
-	    }
-    }
 
     constructor() {
     }
