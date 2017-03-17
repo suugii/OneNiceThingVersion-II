8879a405c5bf99bc7273bfbdaba8cd480c436fc3
N. Tugsbileg <N. Tugsbileg>
2017-03-17 10:52:44 +0800

    add google maps

--- src/app/profile/user-map/user-map.component.ts
+++ src/app/profile/user-map/user-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Component({
   selector: 'app-user-map',
   templateUrl: './user-map.component.html',
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMapComponent implements OnInit {
 
-  constructor() { }
+  lat: number = 40.363870;
+  lng: number = -87.770010;
+  zoom: number = 2;
+  user: string;
+  objects: any;
+
+  constructor(private af: AngularFire) {
+    this.af.auth.subscribe(
+      (auth) => {
+        if (auth) {
+          this.user = auth.uid;
+        }
+      }
+    );
+
+
+    this.objects = this.af.database.list('stories', {
+      query: {
+        orderByChild: 'touser',
+        equalTo: this.user,
+      }
+    });
+  }
 
   ngOnInit() {
   }
