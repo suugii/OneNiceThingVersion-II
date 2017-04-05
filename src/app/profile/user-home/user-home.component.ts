73c9d46a2272a23c1bd2308a418bdfca55218a10
byambaa122 <byambaa.122@gmail.com>
2017-04-05 17:56:23 +0800

    count My page

--- src/app/profile/user-home/user-home.component.ts
+++ src/app/profile/user-home/user-home.component.ts
@@ -4,49 +4,72 @@ import { Subject } from 'rxjs/Subject';
 
 
 @Component({
-  selector: 'app-user-home',
-  templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.css']
+	selector: 'app-user-home',
+	templateUrl: './user-home.component.html',
+	styleUrls: ['./user-home.component.css']
 })
 export class UserHomeComponent implements OnInit {
 
-  lat: number = 40.363870;
-  lng: number = -87.770010;
-  zoom: number = 2;
-  user: string;
-  objects: any;
-  constructor(private af: AngularFire) {
-    this.af.auth.subscribe(
-      (auth) => {
-        if (auth) {
-          this.user = auth.uid;
-        }
-      }
-    );
+	lat: number = 40.363870;
+	lng: number = -87.770010;
+	zoom: number = 2;
+	user: string;
+	objects: any;
+	storiesCount: number = 0;
+	originateCount: number = 0;
+
+	constructor(private af: AngularFire) {
+		this.af.auth.subscribe(
+			(auth) => {
+				if (auth) {
+					this.user = auth.uid;
+				}
+			}
+		);
+		this.af.database.list('stories', {
+			query: {
+				orderByChild: 'user',
+				equalTo: this.user
+			}
+		}).subscribe(
+			dataStory => {
+				this.storiesCount = dataStory.length;
+			}
+		);
+		this.af.database.list('stories', {
+			query: {
+				orderByChild: 'touser',
+				equalTo: this.user
+			}
+		}).subscribe(
+			dataStory => {
+				this.originateCount = dataStory.length;
+			}
+		);
 
 
-    this.af.database.list('stories', {
-      query: {
-        orderByChild: 'user',
-        equalTo: this.user,
-      }
-    }).subscribe(datas => {
-      this.objects = [];
-      datas.forEach(data => {
-        this.af.database.object('users/' + data.touser).subscribe(userData => {
-          data.touser = userData;
-        })
-        this.af.database.object('users/' + data.user).subscribe(userData => {
-          data.user = userData;
-        })
-        this.objects.push(data);
-      })
-    });
+		this.af.database.list('stories', {
+			query: {
+				orderByChild: 'user',
+				equalTo: this.user,
+			}
+		}).subscribe(datas => {
+			this.objects = [];
+			datas.forEach(data => {
+				this.af.database.object('users/' + data.touser).subscribe(userData => {
+					data.touser = userData;
+				})
+				this.af.database.object('users/' + data.user).subscribe(userData => {
+					data.user = userData;
+				})
+				this.objects.push(data);
+			})
+		});
 
 
-  }
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
